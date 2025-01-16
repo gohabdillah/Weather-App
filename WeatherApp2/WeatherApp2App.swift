1ef44612eea42db2fe109755f61940b7a9d8f2d8
abdi <gohabdillah@gmail.com>
2025-01-16 11:02:30 +0800

    Initial Commit

---
+++ WeatherApp2/WeatherApp2App.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp2App.swift
+//  WeatherApp2
+//
+//  Created by abdillah goh on 16/1/25.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WeatherApp2Tests/WeatherApp2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp2Tests.swift
+//  WeatherApp2Tests
+//
+//  Created by abdillah goh on 16/1/25.
+//
+
+import Testing
+@testable import WeatherApp2
+
+struct WeatherApp2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
